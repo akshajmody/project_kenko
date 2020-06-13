b7db5ce6493bc73a8574a62a0395e1d7aa892712
Akshaj Mody <akshajmody@gmail.com>
2020-06-13 06:05:58 -0700

    Add styling

--- client/src/components/MealPlan.jsx
+++ client/src/components/MealPlan.jsx
@@ -23,7 +23,7 @@ class MealPlan extends React.Component {
 
   componentDidMount() {
     const input = this.props.cuisine;
-    setTimeout(() => {this.getFoods(this.props.cuisine)}, 2000);
+    setTimeout(() => {this.getFoods(this.props.cuisine)}, 1500);
   }
 
 
@@ -57,7 +57,7 @@ class MealPlan extends React.Component {
     const input = this.props.cuisine;
     this.setState({renderMealPlan: false})
     //effect of "processing" - purely aesthetic
-    setTimeout(() => {this.getFoods(input)}, 2000);
+    setTimeout(() => {this.getFoods(input)}, 1500);
   }
 
 
@@ -65,31 +65,33 @@ class MealPlan extends React.Component {
     if (this.state.renderMealPlan === true) {
       return (
         <div className="foods">
-          <div className="regenerate" onClick={this.regenerate}>generate new optimization</div>
+          <div className="regenerate" onClick={this.regenerate}>⠕generate new optimization⠪</div>
+          <div className="regenerate">⠕upload optimized meal (UNDER MAINTENANCE)⠪</div>
+          {/* this functionality was going to allow saving of meal plans - in creating this app further, there will be a search function that allows a user to pull up a meal plan based on ID or saved name of the meal plan - this can become somewhat of a social aspect, sharing meal plans with other users, other users can access the meal plans via a code to access from the database! */}
           <div className="carbHover">
             <div className="carbo1">Carbohydrate: {this.state.carbo.carbo} </div>
-            <div className="carbo2 info">Benefits: {this.state.carbo.utility} </div>
+            <div className="carbo2 info">⬡Benefits: {this.state.carbo.utility} </div>
           </div>
           <div className="fatHover">
             <div className="fat1">Fat: {this.state.fat.fat} </div>
-            <div className="fat2 info">Benefits: {this.state.fat.utility} </div>
+            <div className="fat2 info">⬡Benefits: {this.state.fat.utility} </div>
           </div>
           <div className="proteinHover">
             <div className="protein1">Protein: {this.state.protein.protein} </div>
-            <div className="protein2 info">Benefits: {this.state.protein.utility} </div>
+            <div className="protein2 info">⬡Benefits: {this.state.protein.utility} </div>
           </div>
           <div className="vegHover">
             <div className="veg1">Vegetables: {this.state.vegetable[0].vegetable}, {this.state.vegetable[1].vegetable}, {this.state.vegetable[2].vegetable} </div>
-            <div className="veg2 info">Benefits: {this.state.vegetable[0].utility},<br/>{this.state.vegetable[1].utility},<br/>{this.state.vegetable[2].utility} </div>
+            <div className="veg2 info">⬡Benefits: {this.state.vegetable[0].utility},<br/>{this.state.vegetable[1].utility},<br/>{this.state.vegetable[2].utility} </div>
           </div>
           <div>Seasonings: {this.state.seasoning[0].seasoning}, {this.state.seasoning[1].seasoning}, {this.state.seasoning[2].seasoning}, {this.state.seasoning[0].seasoning}</div>
           <div className="proHover">
             <div className="pro1">Probiotic: {this.state.probiotic.probiotic} </div>
-            <a className="pro2 info" href="https://www.health.harvard.edu/vitamins-and-supplements/health-benefits-of-taking-probiotics" target="_blank">Harvard Medical: Benefits of probiotics</a>
+            <a className="pro2 info" href="https://www.health.harvard.edu/vitamins-and-supplements/health-benefits-of-taking-probiotics" target="_blank">⬡Harvard Medical: Benefits of probiotics</a>
           </div>
           <div className="fruitHover">
             <div className="fruit1">Fruits: Meal 1:&nbsp;{this.state.fruit[0].fruit}, Meal 2:&nbsp;{this.state.fruit[1].fruit}</div>
-            <div className="fruit2 info">Benefits: {this.state.fruit[0].utility},<br/>{this.state.fruit[1].utility}</div>
+            <div className="fruit2 info">⬡Benefits: {this.state.fruit[0].utility},<br/>{this.state.fruit[1].utility}</div>
           </div>
           <div className="supHover">
             <div className="sup1">Supplements:
@@ -99,7 +101,7 @@ class MealPlan extends React.Component {
               <div className="supplements">&nbsp;&nbsp;&nbsp;--Digestion Concerns: {this.state.supplement[5]?.supplement}</div>
               {/* MULTIPLE ERRORS HERE - FIXED BY ADDING NULL CHECK BUT NOT SURE WHY CANNOT RENDER ANYTHING BEYOND INDEX 4 FOR ANY ARRAY IN STATE - WITHOUT ? , THIS SUPPLEMENT LINE READS UNDEFINED */}
             </div>
-            <a className="sup2 info" href="https://www.health.harvard.edu/staying-healthy/supplements-a-scorecard" target="_blank"> Harvard Medical Supplement Scorecard <br/> Multivitamins taken first thing with chilled water upon waking up. <br/> Collagen 30 minutes before first meal to help break intermittent fast smoothly.</a>
+            <a className="sup2 info" href="https://www.health.harvard.edu/staying-healthy/supplements-a-scorecard" target="_blank">⬡Harvard Medical Supplement Scorecard <br/> Multivitamins taken first thing with chilled water upon waking up. <br/> Collagen 30 minutes before first meal to help break intermittent fast smoothly.</a>
           </div>
         </div>
       )
